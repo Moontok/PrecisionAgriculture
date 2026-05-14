8e4d4cf9512f9a5297d76371496c3002337c8325
Moontok <zspink@nea.k12.ar.us>
2026-05-14 13:01:09 -0500

    Exclude onboard LED/USB pins from reset
    Add PINS_NO_RESET = [23, 24, 25] and skip those pins when driving pins low in reset_pins(). This prevents resetting the onboard LED and USB-related pins (avoiding disruption of USB communication/indicator) while still clearing other GPIOs.

--- Pico/reset_pins.py
+++ Pico/reset_pins.py
@@ -3,10 +3,13 @@
 import machine as mn
 
 
+PINS_NO_RESET = [23, 24, 25] # These pins are used for the onboard LED and USB communication, so we won't reset them
+
 def reset_pins():
     for i in range(29):
-        pin = mn.Pin(i, mn.Pin.OUT)
-        pin.low()
+        if i not in PINS_NO_RESET:
+            pin = mn.Pin(i, mn.Pin.OUT)
+            pin.low()
 
 
 if __name__ == "__main__":
