05d2c9639b11400d0860f0c5bf4bb26b14183911
Zackary <zspink@nea.k12.ar.us>
2026-06-15 12:24:27 -0500

    Update remote_sensing.py

--- SatData/remote_sensing.py
+++ SatData/remote_sensing.py
@@ -1,9 +1,9 @@
-import rasterio
+import rasterio as rio
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_rgb(red_band_path, green_band_path, blue_band_path):
+def plot_rgb(r_path, g_path, b_path):
     """RGB (Red-Green-Blue):
     This is the most common way to visualize color images.
     It combines the red, green, and blue bands into a single image.
@@ -12,20 +12,22 @@ def plot_rgb(red_band_path, green_band_path, blue_band_path):
     and the blue band for the blue channel.
     """
     
-    with rasterio.open(red_band_path) as red_band:
-        with rasterio.open(green_band_path) as green_band:
-            with rasterio.open(blue_band_path) as blue_band:
-                red = red_band.read(1).astype(float)
-                green = green_band.read(1).astype(float)
-                blue = blue_band.read(1).astype(float)
-                rgb = np.dstack((red, green, blue))
-        rgb = rgb / rgb.max()
+    with rio.open(r_path) as red_band:
+        red = red_band.read(1).astype(float)
+    with rio.open(g_path) as green_band:
+        green = green_band.read(1).astype(float)
+    with rio.open(b_path) as blue_band:
+        blue = blue_band.read(1).astype(float)
+
+    rgb = np.dstack((red, green, blue))
+    rgb = rgb / rgb.max()
+    
     plt.imshow(rgb)
     plt.title('RGB from Landsat Imagery')
     plt.show()
 
 
-def plot_ndvi(red_band_path, nir_band_path):
+def plot_ndvi(r_path, nir_path):
     """NDVI (Normalized Difference Vegetation Index):
     This is the most widely used VI.
     It measures the difference between near-infrared (NIR) and red reflectance,
@@ -34,18 +36,20 @@ def plot_ndvi(red_band_path, nir_band_path):
 
     NDVI = (NIR - Red) / (NIR + Red)
     """
-    with rasterio.open(red_band_path) as red_band:
-        with rasterio.open(nir_band_path) as nir_band:
-            red = red_band.read(1).astype(float)
-            nir = nir_band.read(1).astype(float)            
-            ndvi = (nir - red) / (nir + red)
+    with rio.open(r_path) as red_band:
+        red = red_band.read(1).astype(float)
+    with rio.open(nir_path) as nir_band:
+        nir = nir_band.read(1).astype(float)            
+    
+    ndvi = (nir - red) / (nir + red)
+
     plt.imshow(ndvi, cmap='RdYlGn')
     plt.colorbar(label='NDVI')
     plt.title('NDVI from Landsat Imagery')
     plt.show()
 
 
-def plot_gndvi(green_band_path, nir_band_path):
+def plot_gndvi(g_path, nir_path):
     """GNDVI (Green Normalized Difference Vegetation Index):
     This is a vegetation index that minimizes soil brightness influences.
     It is particularly useful in areas with high soil brightness.
@@ -54,18 +58,20 @@ def plot_gndvi(green_band_path, nir_band_path):
     GNDVI = (NIR - Green) / (NIR + Green)
     """
 
-    with rasterio.open(green_band_path) as green_band_path:
-        with rasterio.open(nir_band_path) as nir_band_path:
-            green = green_band_path.read(1).astype(float)
-            nir = nir_band_path.read(1).astype(float)
-            gndvi = (nir - green) / (nir + green)
+    with rio.open(g_path) as g_path:
+        green = g_path.read(1).astype(float)
+    with rio.open(nir_path) as nir_path:
+        nir = nir_path.read(1).astype(float)
+    
+    gndvi = (nir - green) / (nir + green)
+
     plt.imshow(gndvi, cmap='RdYlGn')
     plt.colorbar(label='Green Normalized Difference Vegetation Index (GNDVI)')
     plt.title('GNDVI from Landsat Imagery')
     plt.show()
 
 
-def plot_ndmi(swir1_band_path, nir_band_path):
+def plot_ndmi(swir1_path, nir_path):
     """NDMI (Normalized Difference Moisture Index):
     This is a vegetation index that is sensitive to moisture content.
     It compares the near-infrared (NIR) and shortwave infrared (SWIR) bands.
@@ -73,18 +79,20 @@ def plot_ndmi(swir1_band_path, nir_band_path):
     NDMI = (NIR - SWIR1) / (NIR + SWIR1)
     """
 
-    with rasterio.open(nir_band_path) as nir_band_path:
-        with rasterio.open(swir1_band_path) as swir1_band_path:
-            nir = nir_band_path.read(1).astype(float)
-            swir1 = swir1_band_path.read(1).astype(float)
-            ndmi = (nir - swir1) / (nir + swir1)
+    with rio.open(nir_path) as nir_path:
+        nir = nir_path.read(1).astype(float)
+    with rio.open(swir1_path) as swir1_path:
+        swir1 = swir1_path.read(1).astype(float)
+    
+    ndmi = (nir - swir1) / (nir + swir1)
+
     plt.imshow(ndmi, cmap='RdYlGn')
     plt.colorbar(label='NDMI')
     plt.title('NDMI from Landsat Imagery')
     plt.show()
 
 
-def plot_ndwi(green_band, nir_band):
+def plot_ndwi(g_band, nir_band):
     """NDWI (Normalized Difference Water Index):
     This is a vegetation index that is sensitive to water content.
     It compares the green and near-infrared (NIR) bands.
@@ -92,11 +100,13 @@ def plot_ndwi(green_band, nir_band):
     NDWI = (Green - NIR) / (Green + NIR)
     """
 
-    with rasterio.open(green_band) as green_band:
-        with rasterio.open(nir_band) as nir_band:
-            green = green_band.read(1).astype(float)
-            nir = nir_band.read(1).astype(float)
-            ndwi = (green - nir) / (green + nir)
+    with rio.open(g_band) as g_band:
+        green = g_band.read(1).astype(float)
+    with rio.open(nir_band) as nir_band:
+        nir = nir_band.read(1).astype(float)
+
+    ndwi = (green - nir) / (green + nir)
+
     plt.imshow(ndwi, cmap='RdYlGn')
     plt.colorbar(label='NDWI')
     plt.title('NDWI from Landsat Imagery')
@@ -110,9 +120,9 @@ nir_band = "may_data/B5.TIF"
 swir1_band = "may_data/B6.TIF"
 
 # plot_rgb(red_band, green_band, blue_band)
-# plot_ndvi(red_band, nir_band)
+plot_ndvi(red_band, nir_band)
 # plot_gndvi(green_band, nir_band)
 # plot_ndmi(swir1_band, nir_band)
-plot_ndwi(green_band, nir_band)
+# plot_ndwi(green_band, nir_band)
 
 
